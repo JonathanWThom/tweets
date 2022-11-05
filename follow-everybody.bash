63b99801bed1811312cfa75fbf40e2b8c69dce72
Jonathan Thom <jonathanthom@hey.com>
2022-11-05 16:19:45 -0700

    I realized the follow-everybody script wasn't getting all users
    It was paginating at 30. Switching to the gh cli because it makes
    pagination super easy. Wondering if github actions will have this
    installed by default or not..

--- follow-everybody.bash
+++ follow-everybody.bash
@@ -1,25 +1,10 @@
 # Copyright @datn 2022. Heavens forgive me for this
 # be in your repo dir for this
 
-declare FORKS
-page=1
-
-get_forks() {
-	FORKS=$(curl -H "Accept: application/vnd.github+json" "https://api.github.com/repos/diracdeltas/tweets/forks?per_page=100&page=$page")
-
-	for I in `$FORKS | jq -r '.[].full_name'`
-	do
-		USEW=`echo $I | cut -d\/ -f1`
-		WEPO=`echo $I | cut -d\/ -f2`
-		git remote add $USEW https://github.com/$USEW/$WEPO
-		git fetch $USEW
-	done
-}
-
-while [$FORKS != ""]
+for I in `gh api https://api.github.com/repos/diracdeltas/tweets/forks --paginate | jq -r '.[].full_name'`
 do
-	get_forks
-	$page+=1
+	USEW=`echo $I | cut -d\/ -f1`
+	WEPO=`echo $I | cut -d\/ -f2`
+	git remote add $USEW https://github.com/$USEW/$WEPO
+	git fetch $USEW
 done
-
-
